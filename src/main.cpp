c9a0730622442249a11fb18855403ad9ef074ab4
Couto1411 <gabriel.couto14@hotmail.com>
2022-09-09 08:03:59 -0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,13 @@ int main(){
 	Tree *T = new Tree;
 	Hash *map = new Hash;
 	string nomeArquivo;
-	printf("Escreva o nome dos arquivos:\n");
+	printf("Escreva o nome do arquivo:\n");
     cin>>nomeArquivo;
 	map->preencheMap(nomeArquivo);
 	map->normalize();
 	map->createTree(&T);
 	AlturaArvoreLargura(&altura,&T);
+	printf("A árvore das palavras calculadas com o algoritmo de huffman tem altura %d\n", altura);
 	writeFile(&T,nomeArquivo);
 
 	return 0;
